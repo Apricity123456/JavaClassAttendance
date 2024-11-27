9bf87e59abe3241c7fa3366bfd516433e7c7ea4d
xjp0316 <xjpppppp0316@gmail.com>
2024-11-27 10:44:28 +0100

    Update Class.java

--- src/main/java/models/Class.java
+++ src/main/java/models/Class.java
@@ -1,8 +1,39 @@
+// 文件路径: src/main/java/models/Class.java
 package models;
 
-public class Class {
-    private int id;
-    private String name;
 
-    // Getter 和 Setter 方法
+public class Class {
+    private int id;      // Class ID
+    private String name; // Class name
+
+    public Class() {}
+
+    public Class(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
