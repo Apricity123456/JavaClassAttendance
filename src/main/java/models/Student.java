5caa56764761cc30de3bca81716be3db43916cef
Shangyu YANG <shangyu.yang@eleve.isep.fr>
2024-12-16 21:16:01 +0100

    rebuild structrue

--- src/main/java/models/Student.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class Student {
-    private int id;
-    private String name;
-    private int classId;
-
-    // Getter 和 Setter 方法
-}
--- src/main/java/view/MainApp.java
+++ src/main/java/view/MainApp.java
@@ -1,13 +1,14 @@
+package view;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/LoginView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/LoginView.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setTitle("Class Management System");
         primaryStage.setScene(scene);
